72e2be576039d5234ffd5280a045647ba84e750d
root <root@ip-172-31-7-64.eu-west-2.compute.internal>
2023-03-12 05:33:47 +0000

    my first cli commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. to see the world
+          Edit <code>src/App.js</code> welcome to the santy world
         </p>
         <a
           className="App-link"
